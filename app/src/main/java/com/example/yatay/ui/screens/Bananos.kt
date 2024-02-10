7d495ae96f4a252e3684c2b5d2240b8c26dd009e
Germansdev <germanschw@yahoo.com>
2024-02-10 10:28:55 -0300

    Calendar: set tittle and headline

--- app/src/main/java/com/example/yatay/ui/screens/Bananos.kt
+++ app/src/main/java/com/example/yatay/ui/screens/Bananos.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -51,10 +52,10 @@ fun Bananos(
 
     Column(
         modifier = Modifier
-
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(end = 8.dp)
+            .fillMaxSize()
+           // .fillMaxWidth()
+           // .fillMaxHeight()
+           // .padding(end = 8.dp)
     ) {
 
             Card(
--- app/src/main/java/com/example/yatay/ui/screens/DateRangePicker.kt
+++ app/src/main/java/com/example/yatay/ui/screens/DateRangePicker.kt
@@ -1,6 +1,5 @@
 package com.example.yatay.ui.screens
 
-import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,19 +11,24 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DateRangePickerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import java.util.Calendar
+import androidx.compose.ui.zIndex
+import kotlinx.coroutines.launch
 
 
 /**
@@ -53,9 +57,9 @@ fun DateRangePicker(
 ) {
 
 // Decoupled snackbar host state from scaffold state for demo purposes.
-   // val snackState = remember { SnackbarHostState() }
+    val snackState = remember { SnackbarHostState() }
     val snackScope = rememberCoroutineScope()
-  //  SnackbarHost(hostState = snackState, Modifier.zIndex(1f))
+    SnackbarHost(hostState = snackState, Modifier.zIndex(1f))
     val state = rememberDateRangePickerState()//rememberDateRangePickerState()
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top) {
         // Add a row with "Save" and dismiss actions.
@@ -72,12 +76,13 @@ fun DateRangePicker(
             }
             TextButton(
                 onClick = {
-                /**    snackScope.launch {
+                    snackScope.launch {
                         snackState.showSnackbar(
-                            "Saved range (timestamps): " +
-                                    "${state.selectedStartDateMillis!!..state.selectedEndDateMillis!!}"
+                            "Saved range (timestamps): " /**+
+                                    "${state.selectedStartDateMillis!!..state.selectedEndDateMillis!!}"*/
+
                         )
-                    }*/
+                    }
                     onClick()
                 },
                 enabled = state.selectedEndDateMillis != null,
@@ -92,6 +97,24 @@ fun DateRangePicker(
 
         DateRangePicker(
             state = state,
+            title = { Text(text = "Select dates") },
+            headline = {
+
+                DateRangePickerDefaults.DateRangePickerHeadline(
+                    selectedStartDateMillis =  state.selectedStartDateMillis ,
+                    selectedEndDateMillis = state.selectedEndDateMillis,
+                    displayMode = state.displayMode,
+                    dateFormatter = remember { DatePickerDefaults.dateFormatter(
+                        yearSelectionSkeleton =  "yy/MM",//"Start Date:" + if(state.selectedStartDateMillis!=null) state.selectedStartDateMillis?.let { getFormattedDate(it) } else "",
+                        selectedDateSkeleton = "dd/MM",//"YYYY",
+                       selectedDateDescriptionSkeleton = ""
+
+                    )
+                                             },
+
+                    modifier = Modifier
+                        .padding() )
+            },
             colors = DatePickerDefaults.colors(
                 containerColor = Color.Blue,
                 titleContentColor = Color.Black,
@@ -107,13 +130,25 @@ fun DateRangePicker(
                 dayInSelectionRangeContentColor = Color.White,
                 selectedDayContainerColor = Color.Black
             ),
+            showModeToggle = false,
             modifier = Modifier.weight(1f))
     }
 }
 
-fun getFormattedDate(timeInMillis: Long): String{
-    val calender = Calendar.getInstance()
-    calender.timeInMillis = timeInMillis
-    val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-    return dateFormat.format(calender.timeInMillis)
+/**
+ * PENDING: SET LANGUAGE CALENDAR:
+ */
+/**
+@ExperimentalMaterial3Api
+fun DateRangePickerState(
+    locale: CalendarLocale,
+    initialSelectedStartDateMillis: Long? = null,
+    initialSelectedEndDateMillis: Long? = null,
+    initialDisplayedMonthMillis: Long? = initialSelectedStartDateMillis,
+    yearRange: IntRange = DatePickerDefaults.YearRange,
+    initialDisplayMode: DisplayMode = DisplayMode.Picker,
+    selectableDates: SelectableDates = DatePickerDefaults.AllDates
+): DateRangePickerState {
+    locale = CalendarLocale(language = "")
 }
+*/
